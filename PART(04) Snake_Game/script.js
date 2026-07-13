917aa1b39958492c6b60b71200e25dc7cb4968ad
SAMRAT MAURYA <121427185+samratkartikey@users.noreply.github.com>
2026-07-13 15:15:25 +0530

    Add direction change restrictions for snake
    Prevent snake from reversing direction directly.

--- PART(04) Snake_Game/script.js
+++ PART(04) Snake_Game/script.js
@@ -210,25 +210,20 @@ function restartGame(){
 
 
 addEventListener("keydown",(event)=>{
-    if(event.key=="ArrowUp")
+    if(event.key==="ArrowUp" && direction!=="down")
     {
         direction="up";
     }
-    else if(event.key=="ArrowRight")
+    else if(event.key==="ArrowRight" && direction!=="left")
     {
         direction="right";
     }
-    else if(event.key=="ArrowDown")
+    else if(event.key==="ArrowDown" && direction!=="up")
     {
         direction="down";
     }
-    else if(event.key=="ArrowLeft")
+    else if(event.key=="ArrowLeft" && direction!=="right")
     {
         direction="left";
     }
 })
-
-
-
-
-
